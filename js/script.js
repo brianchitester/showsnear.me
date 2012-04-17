934d15149ef8344d5878b3ecfa3823acbb6f4f6d
Brian Chitester <yagoogaly@gmail.com>
2012-04-17 13:41:55 -0400

    Markers on google map fixed

--- js/script.js
+++ js/script.js
@@ -26,6 +26,12 @@ function main(){
 
 		/*draw the map*/
 		var map = new google.maps.Map(document.getElementById("mapcanvas"),myOptions);
+		
+		/*Info window init for google maps */
+		var infowindow = new google.maps.InfoWindow({ 
+			content: "holding...",
+			size: new google.maps.Size(50,50)
+		});
 
 		/*do last.fm stuff*/
 		lastfm.geo.getEvents({lat: lat, long: lon}, {success: function(data){
@@ -36,12 +42,40 @@ function main(){
 			// this for loop displays information for the first 10 events returned by lastfm.geo.getEvents
 			// default listing is by date
 			for (var i = 0; i < 10; i++){
+			
+				//shorten the date - remove time, use data.events.event[i].startTime instead
+				var length = data.events.event[i].startDate.length;
+				var shortDate = data.events.event[i].startDate.substring(0,length-9);
+				
+				//display events
 				$('#mainContent').append( 
 					"<h3>" + data.events.event[i].title + "</h3>" +
+ 					"<p>" + shortDate + " at " +
+					data.events.event[i].venue.name + " in " +
+					data.events.event[i].venue.location.city + "</p>"
+				);
+				
+				/* Create markers to Google map */ 
+				//REFERENCE: 
+				//https://developers.google.com/maps/documentation/javascript/event
+				//http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+				var location = new google.maps.LatLng(data.events.event[i].venue.location["geo:point"]["geo:lat"], data.events.event[i].venue.location["geo:point"]["geo:long"]);
+			    var marker = new google.maps.Marker({
+			        position: location, 
+			        title: data.events.event[i].title,
+					html: "<h3>" + data.events.event[i].title + "</h3>" +
  					"<p>" + data.events.event[i].startDate + "</p>" +
 					"<p>" + data.events.event[i].venue.name + "</p>" +
 					"<p>" + data.events.event[i].venue.location.city + "</p>"
-				);
+			    });
+				
+			    google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(this.html);
+					infowindow.open(map,this);
+			    });
+				marker.setMap(map);
+
+					
 			}
 
 			/*printing some extra data can be helpful*/
